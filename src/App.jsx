d10144b0d015d4da222fc0c4563581fdd64109d1
Silas Alvares <83738111+cxryoff@users.noreply.github.com>
2025-05-24 14:08:33 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+export default function App() {
+  return (
+    <div>
+      <h1>NeuroCode</h1>
+    </div>
