217648bf804dc4907ef3da86c19640cfb1a47fa1
Silas Alvares <83738111+cxryoff@users.noreply.github.com>
2025-05-24 14:35:46 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import './index.css'
 
 export default function App() {
   return (
-    <div>
-      <h1>NeuroCode</h1>
+    <div className="app">
+      <h1>NeuroCode 🧠</h1>
+      <p>Seu site está funcionando perfeitamente!</p>
+      <div className="logo">
+        <img src="/vite.svg" alt="Vite Logo" width="100" />
+      </div>
     </div>
+  )
+}
